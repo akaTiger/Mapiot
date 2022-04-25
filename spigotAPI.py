ed609aac2ee4c3c702ca88a741b270f61be8f2fb
Tiger <ding@tiger.work>
2022-04-25 14:51:17 -0700

    Update spigotAPI.py

--- spigotAPI.py
+++ spigotAPI.py
@@ -1,5 +1,66 @@
+import lib
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+
 class spigotTracker(object):
     def __init__(self, mainDisplay):
-        self.main = mainDisplay
-        self._funcName = "Spigot API Tracker"
-        self._head = ""
+        self._main = mainDisplay
+        self.funcName = "Spigot API Tracker"
+        self._head = "https://api.spigotmc.org/simple/0.2/index.php"
+        self._nameLib = [i[0] for i in lib.getParameters().values()]
+        self._funcLib = lib.getParameters()
+        self.initFrame()
+    
+    def initFrame(self):
+        if True:
+            # Upper
+            self._displayFrame = Frame(self._main)
+            self._displayFrame.pack(fill="both", expand="yes")
+            
+        if True:
+            # input Frame
+            self._inputFrame = Frame(self._main, height=150)
+            self._inputFrame.pack(fill="x")
+            # input block
+            self._lF = LabelFrame(self._inputFrame, text="Filters", padx=10, pady=10)
+            self._lF.pack(fill="both", padx=10, pady=10)
+        
+        if True:
+            # scroll text
+            self._disbox = ScrolledText(self._displayFrame)
+            self._disbox.pack(fill="both", expand="yes", padx=10, pady=10)
+        
+        if True:
+            self._funcChoice = StringVar()
+        if True:
+            # options widget
+            OptionMenu(self._lF, self._funcChoice, *self._nameLib).pack(expand="yes", fill="x")
+            Button(self._lF, text="Execute", command=self.query).pack(expand="yes", fill="x")
+    
+    def debug(self):
+        print(self._funcChoice.get())
+    
+    def query(self):
+        choice = self._funcChoice.get()
+        if self._funcLib[0][0] == choice:
+            self.api(self._head + "?action=" + self._funcLib[0][1])
+        elif self._funcLib[1][0] == choice:
+            self.api(self._head + "?action=" + self._funcLib[1][1])
+        elif self._funcLib[2][0] == choice:
+            self.api(self._head + "?action=" + self._funcLib[2][1])
+        elif self._funcLib[3][0] == choice:
+            self.api(self._head + "?action=" + self._funcLib[3][1])
+        elif self._funcLib[4][0] == choice:
+            self.api(self._head + "?action=" + self._funcLib[4][1])
+        elif self._funcLib[5][0] == choice:
+            self.api(self._head + "?action=" + self._funcLib[5][1])
+        elif self._funcLib[6][0] == choice:
+            self.api(self._head + "?action=" + self._funcLib[6][1])
+        elif self._funcLib[7][0] == choice:
+            self.api(self._head + "?action=" + self._funcLib[7][1])
+    
+    def api(self, url):
+        if True:
+            self._disbox.delete("1.0", "end")
+            self._disbox.insert(INSERT, url)
+        
