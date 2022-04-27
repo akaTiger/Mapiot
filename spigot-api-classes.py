ab72bbd3faa22a00f0cbe69fd0fd789dbc586d59
Tiger <ding@tiger.work>
2022-04-26 23:31:20 -0700

    Create spigot-api-classes.py

---
+++ spigot-api-classes.py
@@ -0,0 +1,16 @@
+class listResources(object):
+    pass
+class getResource(object):
+    pass
+class getResourcesByAuthor(object):
+    pass
+class listResourceCategories(object):
+    pass
+class getResourceUpdate(object):
+    pass
+class getResourceUpdates(object):
+    pass
+class getAuthor(object):
+    pass
+class findAuthor(object):
+    pass
